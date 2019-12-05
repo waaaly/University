18fee710a2edc741bf2e56e516218a25caa4d922
waaaly <825887013@qq.com>
2019-12-05 19:01:35 +0800

    1、新增订单状态、版本号提示； 2、修复版本管理器、商家评论，订单组件。 3、升级版本号V0.2.4.
    Signed-off-by: waaaly <825887013@qq.com>

--- code/src/utils/updateManager.js
+++ code/src/utils/updateManager.js
@@ -16,28 +16,26 @@ const checkUpdate = function() {
 
 //下载新版本
 const dowloadNewVersion = function() {
+    console.log('正在下载新版本。。。');
     wx.showLoading({ title: '正在下载新版本' });
-    //下载成功
-    updateManager.onUpdateReady(() => {
+    return new Promise((resolve, reject) => {
+        //下载成功
+        updateManager.onUpdateReady(() => {
+            console.log('新版下载成功！');
             wx.hideLoading();
-            wx.showModal({
-                title: '下载成功',
-                content: `新版本已下载完成，点击下方重启按钮，体验新功能吧～`,
-                showCancel: false,
-                comfireText: '立即重启',
-                success: (res => {
-                    if (res.comfirm) {
-                        updateManager.applyUpdate();
-                    }
-                })
-            })
+            resolve();
         })
+
         //下载失败
-    updateManager.onUpdateFailed(() => {
-        wx.showModal({
-            title: '下载失败',
-            content: `由于不知名的原因，新版本下载失败了，请您删除当前小程序，重新搜索打开哟~`,
+        updateManager.onUpdateFailed(() => {
+            console.log('新版下载失败！');
+            wx.hideLoading();
+            reject();
         })
     })
 }
-module.exports = checkUpdate, dowloadNewVersion;
+
+const restart = function() {
+    updateManager.applyUpdate();
+}
+module.exports = checkUpdate, dowloadNewVersion, restart;
